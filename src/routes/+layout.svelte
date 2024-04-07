f5ab4a1312294f7318b481cf012b8278333571a0
Zac Pullar-Strecker <zac.pullar-strecker@lanzatech.com>
2024-04-08 10:39:51 +1200

    Disable manifest file
    For some reason after the manifest loads JS chunks fail to load (they
    attempt to load from /chatui/_app/... instead of /chat/_app/...). I
    don't know why this is. Disabling the manifest is a hack but works for
    now.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -164,11 +164,11 @@
 		href="{envPublic.PUBLIC_ORIGIN ||
 			$page.url.origin}{base}/{envPublic.PUBLIC_APP_ASSETS}/apple-touch-icon.png"
 	/>
-	<link
+	<!-- <link
 		rel="manifest"
 		href="{envPublic.PUBLIC_ORIGIN ||
 			$page.url.origin}{base}/{envPublic.PUBLIC_APP_ASSETS}/manifest.json"
-	/>
+	/> -->
 
 	{#if envPublic.PUBLIC_PLAUSIBLE_SCRIPT_URL && envPublic.PUBLIC_ORIGIN}
 		<script
